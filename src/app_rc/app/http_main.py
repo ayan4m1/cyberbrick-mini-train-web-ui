2d612f80a067062bfe5efc8b14c588ef3258fa27
ayan4m1 <andrew@bulletlogic.com>
2026-01-18 19:14:58 -0500

    feat: remove all sim/rc code and update boot.py product name/version

---
+++ src/app_rc/app/http_main.py
@@ -0,0 +1,111 @@
+# -*-coding:utf-8-*-
+#
+# The CyberBrick Codebase License, see the file LICENSE for details.
+#
+# Copyright (c) 2025 MakerWorld
+#
+
+import sys
+import gc
+import machine
+import uasyncio
+import time
+import ulogger
+
+sys.path.append("/app")
+sys.path.append("/bbl")
+if '.frozen' in sys.path:
+    sys.path.remove('.frozen')
+    sys.path.append('.frozen')
+
+wifi_ssid = 'qux'        # WiFi station ID
+wifi_psk = 'wi9NNYara'   # WiFi pre-shared key
+train_speeds = [0, 0, 0, 0]
+
+class Clock(ulogger.BaseClock):
+    def __init__(self):
+        self.start = time.time()
+
+    def __call__(self) -> str:
+        inv = time.time() - self.start
+        return '%d' % (inv)
+
+async def main():
+    rst_c = machine.reset_cause()
+    log_clock = Clock()
+    log_handler_to_term = ulogger.Handler(
+        level=ulogger.INFO,
+        colorful=True,
+        fmt="&(time)%-&(level)%-&(msg)%",
+        clock=log_clock,
+        direction=ulogger.TO_TERM,
+    )
+    log_handler_to_file = ulogger.Handler(
+        level=ulogger.INFO,
+        fmt="&(time)%-&(level)%-&(msg)%",
+        clock=log_clock,
+        direction=ulogger.TO_FILE,
+        file_name="./log/logging",
+        index_file_name="./log/log_index.txt",
+        max_file_size=10240
+    )
+    logger = ulogger.Logger(name=__name__, handlers=(log_handler_to_term, log_handler_to_file))
+    rc2str = {
+        getattr(machine, i): i
+        for i in ('PWRON_RESET',
+                  'HARD_RESET',
+                  'WDT_RESET',
+                  'DEEPSLEEP_RESET',
+                  'SOFT_RESET')
+    }
+    reset_cause = rc2str.get(rst_c, str(rst_c))
+
+    logger.info(f'[MAIN]{reset_cause}')
+    del rc2str
+
+    from network import WLAN, STA_IF
+    wlan = WLAN(STA_IF)
+    wlan.active(True)
+
+    while not wlan.isconnected():
+        logger.info("[WIFI]Retry connect")
+        wlan.connect(wifi_ssid, wifi_psk)
+        await uasyncio.sleep(5)
+    logger.info("[WIFI]Connected!")
+    gc.collect()
+
+    from microdot import Microdot
+    http_server = Microdot()
+
+    @http_server.route('/')
+    async def index():
+        global train_speeds
+
+        return f'''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mini Train RC Control</title>
+    </head>
+    <body>
+        <h1>Mini Train RC Control</h1>
+        <form action="/control" method="POST">
+            <p>Train 1 @ <input type="number" min="0" max="100" step="5" name="rpmList" value="{train_speeds[0]}" />% RPM</p>
+            <p>Train 2 @ <input type="number" min="0" max="100" step="5" name="rpmList" value="{train_speeds[1]}" />% RPM</p>
+            <p>Train 3 @ <input type="number" min="0" max="100" step="5" name="rpmList" value="{train_speeds[2]}" />% RPM</p>
+            <p>Train 4 @ <input type="number" min="0" max="100" step="5" name="rpmList" value="{train_speeds[3]}" />% RPM</p>
+            <p><button type="submit">Update</button></p>
+        </form>
+    </body>
+</html>
+'''
+
+    @http_server.route('/control', methods=['POST'])
+    async def control():
+        return f'''OK'''
+
+    http_server.run()
+
+
+if __name__ == "__main__":
+    uasyncio.run(main())
--- src/app_rc/boot.py
+++ src/app_rc/boot.py
@@ -9,8 +9,8 @@
 import bbl_product
 import sys
 
-_PRODUCT_NAME = "RC"
-_PRODUCT_VERSION = "01.00.00.21"
+_PRODUCT_NAME = "Mini Train"
+_PRODUCT_VERSION = "01.00.00.01"
 
 bbl_product.set_app_name(_PRODUCT_NAME)
 bbl_product.set_app_version(_PRODUCT_VERSION)
@@ -19,6 +19,6 @@ del _PRODUCT_NAME
 del _PRODUCT_VERSION
 
 sys.path.append('/app')
-import rc_main
+import http_main
 
-rc_main.main()
+http_main.main()
